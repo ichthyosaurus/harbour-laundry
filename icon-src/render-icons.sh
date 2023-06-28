12810f0892da656b13302624be7a08d1cab4b17b
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2023-06-28 22:45:29 +0200

    Add banner sources

--- icon-src/render-icons.sh
+++ icon-src/render-icons.sh
@@ -2,14 +2,14 @@
 #
 # This file is part of Opal and has been released into the public domain.
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2021 Mirian Margiani
+# SPDX-FileCopyrightText: 2021-2023 Mirian Margiani
 #
 # See https://github.com/Pretty-SFOS/opal/blob/main/snippets/opal-render-icons.md
 # for documentation.
 #
 # @@@ keep this line: based on template v0.3.0
 #
-c__FOR_RENDER_LIB__="0.3.0"
+c__FOR_RENDER_LIB__="1.0.0"
 
 # Run this script from the same directory where your icon sources are located,
 # e.g. <app>/icon-src.
@@ -22,14 +22,31 @@ for i in raw/*.svg; do
     fi
 done
 
-cNAME="app icons"
-cITEMS=(harbour-laundry)
+cMY_APP=harbour-laundry
+
+cNAME="app icon"
+cITEMS=("$cMY_APP")
 cRESOLUTIONS=(86 108 128 172)
 cTARGETS=(../icons/RESXxRESY)
 render_batch
 
+cNAME="banner image"
+cITEMS=(../dist/banner)
+cRESOLUTIONS=(
+    1080x540++-large
+    540x270++-small
+)
+cTARGETS=(../dist)
+render_batch
+
+cNAME="store icon"
+cITEMS=("$cMY_APP")
+cRESOLUTIONS=(172)
+cTARGETS=(../dist)
+render_batch
+
 cNAME="other graphics"
-cITEMS=(harbour-laundry@256
+cITEMS=("$cMY_APP@256"
         icon-cover-mark-ok@56
 )
 cRESOLUTIONS=(F1)
