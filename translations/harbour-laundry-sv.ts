062f50bbf2bdc845fe8db89b8a0016dccb4243c0
Åke Engelbrektson <eson57@users.noreply.github.com>
2023-10-30 10:58:18 +0100

    Create harbour-laundry-sv.ts
    Add Swedish translation

---
+++ translations/harbour-laundry-sv.ts
@@ -0,0 +1,406 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="19"/>
+        <source>A simple app to keep track of your laundry.</source>
+        <translation>En enkel app som håller koll på din tvätt.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="50"/>
+        <source>Why this app?</source>
+        <translation>Varför denna app?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="51"/>
+        <source>This app fills a very specific niche.</source>
+        <translation>Appen fyller en väldigt specifik nich.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="52"/>
+        <source>If you live in an apartment building that has shared washing machines and a common drying room for everyone, then this app may be for you. If sometimes laundry happens to mysteriously disappear from these places, then this app helps you keep track of questions like “How many items have I washed?” and “How many pieces did I already fetch from the dryer?”.</source>
+        <translation>Om du bor i ett flerfamiljshus som har gemensam tvättstuga med ett gemensamt torkrum, då kan den här appen vara något för dig. Om tvätten ibland råkar försvinna mystiskt från dessa platser, hjälper den här appen dig att hålla reda på frågor som &quot;Hur många plagg har jag tvättat?&quot; och &quot;Hur många plagg har jag redan hämtat från torktumlaren?&quot;.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="61"/>
+        <source>Grocery list</source>
+        <translation>Inköpslista</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="62"/>
+        <source>This app can be used for different purposes.</source>
+        <translation>Den här appen kan användas för olika ändamål.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="63"/>
+        <source>Apart from its original use-case, this app proved to be quite useful as a grocery list. I am currently planning a new version for shopping with mostly cosmetic changes.</source>
+        <translation>Bortsett från det ursprungliga användningsfallet visade sig den här appen vara ganska användbar som inköpslista. Jag planerar för närvarande en ny version för shopping med mestadels kosmetiska förändringar.</translation>
+    </message>
+</context>
+<context>
+    <name>AddItemDialog</name>
+    <message>
+        <location filename="../qml/pages/AddItemDialog.qml" line="69"/>
+        <source>Add</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddItemDialog.qml" line="69"/>
+        <source>Rename</source>
+        <translation>Byt namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddItemDialog.qml" line="75"/>
+        <source>Add a new laundry item:</source>
+        <translation>Lägg till nytt tvättobjekt:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddItemDialog.qml" line="75"/>
+        <source>Rename a laundry item:</source>
+        <translation>Byt namn på tvättobjekt:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddItemDialog.qml" line="84"/>
+        <source>Pants (dark)</source>
+        <translation>Byxor (mörka)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddItemDialog.qml" line="85"/>
+        <source>Descriptive name</source>
+        <translation>Beskrivande namn</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/AddItemDialog.qml" line="159"/>
+        <source>... and %n more</source>
+        <translation>
+            <numerusform>... och %n till</numerusform>
+            <numerusform>... och %n till</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddItemDialog.qml" line="169"/>
+        <source>Note: as the laundry list is sorted alphabetically, it is recommended to group items by type and add details in brackets.
+
+	Example:
+		Pants (dark)
+		Pants (light)
+
+	instead of:
+		dark pants
+		light pants</source>
+        <translation>OBS! Eftersom tvättlistan är sorterad i alfabetisk ordning rekommenderas du att gruppera artiklar efter typ och lägga till detaljer inom parentes.
+
+	Exempel:
+		Byxor (mörka)
+		Byxor (ljusa)
+
+	istället för:
+		Mörka byxor
+		Ljusa byxor</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="56"/>
+        <source>Laundry</source>
+        <translation>Tvätt</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="108"/>
+        <source>No laundry today</source>
+        <translation>Ingen tvätt idag</translation>
+    </message>
+</context>
+<context>
+    <name>LicenseListPart</name>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="119"/>
+        <source>License text</source>
+        <translation>Licenstext</translation>
+    </message>
+</context>
+<context>
+    <name>ListSectionHeader</name>
+    <message>
+        <location filename="../qml/components/ListSectionHeader.qml" line="41"/>
+        <source>Out</source>
+        <translation>Ute</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/ListSectionHeader.qml" line="49"/>
+        <source>Fetched</source>
+        <translation>Hämtat</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="35"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="39"/>
+        <source>Show unused items</source>
+        <translation>Visa oanvända objekt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="39"/>
+        <source>Hide unused items</source>
+        <translation>Dölj oanvända objekt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="44"/>
+        <source>New item</source>
+        <translation>Nytt objekt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="56"/>
+        <source>New batch</source>
+        <translation>Ny tvättomgång</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="77"/>
+        <source>Show archived entries</source>
+        <translation>Visa arkiverade poster</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="77"/>
+        <source>Hide archived entries</source>
+        <translation>Dölj arkiverade poster</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="103"/>
+        <source>Filter</source>
+        <translation>Filter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="130"/>
+        <source>No match</source>
+        <translation>Ingen träff</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="131"/>
+        <source>Pull down to add a new item to the list.</source>
+        <translation>Dra neråt för att lägga till ett nytt objekt i listan.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="138"/>
+        <source>No laundry</source>
+        <translation>Ingen tvätt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="140"/>
+        <source>Pull down to add items.</source>
+        <translation>Dra neråt för att lägga till objekt.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="141"/>
+        <source>Pull down to add a batch.</source>
+        <translation>Dra neråt för att lägga till en ny tvättomgång.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="212"/>
+        <source>Found!</source>
+        <translation>Hittat!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="212"/>
+        <source>Missing!</source>
+        <translation>Saknas!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="219"/>
+        <source>Rename</source>
+        <translation>Byt namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="236"/>
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+</context>
+<context>
+    <name>Opal.About</name>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="55"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="84"/>
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="100"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="22"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="31"/>
+        <source>Development</source>
+        <translation>Utveckling</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="103"/>
+        <source>show contributors</source>
+        <translation>Visa deltagare</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="105"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="51"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="57"/>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="47"/>
+        <source>Homepage</source>
+        <translation>Hemsida</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="108"/>
+        <location filename="../qml/modules/Opal/About/private/ChangelogPage.qml" line="16"/>
+        <source>Changelog</source>
+        <translation>Ändringslogg</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="111"/>
+        <source>Translations</source>
+        <translation>Översättningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="114"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="50"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="58"/>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="50"/>
+        <source>Source Code</source>
+        <translation>Källkod</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="123"/>
+        <source>Donations</source>
+        <translation>Donationer</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="129"/>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="134"/>
+        <source>show license(s)</source>
+        <translation>
+            <numerusform>Visa licens</numerusform>
+            <numerusform>Visa licenser</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/InfoSection.qml" line="15"/>
+        <source>show details</source>
+        <translation>Visa detaljer</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="21"/>
+        <source>Contributors</source>
+        <translation>Deltagare</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="37"/>
+        <source>Acknowledgements</source>
+        <translation>Erkännanden</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="44"/>
+        <source>Thank you!</source>
+        <translation>Tack!</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="48"/>
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="31"/>
+        <source>Details</source>
+        <translation>Detaljerat</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="15"/>
+        <source>External Link</source>
+        <translation>Extern länk</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="32"/>
+        <source>Open in browser</source>
+        <translation>Öppna i webbläsaren</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="36"/>
+        <source>Copied to clipboard: %1</source>
+        <translation>Kopierat till urklipp: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="41"/>
+        <source>Copy to clipboard</source>
+        <translation>Kopiera till urklipp</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="25"/>
+        <source>Download license texts</source>
+        <translation>Ladda ner licenstexter</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="31"/>
+        <source>License(s)</source>
+        <translation>
+            <numerusform>Licens</numerusform>
+            <numerusform>Licenser</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="41"/>
+        <source>Note: please check the source code for most accurate information.</source>
+        <translation>Notis: Kolla källkoden för mest aktuell information.</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/ChangelogNews.qml" line="60"/>
+        <source>News</source>
+        <translation>Nyheter</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/ChangelogNews.qml" line="61"/>
+        <source>Changes since version %1</source>
+        <translation>Ändringar sedan version %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="120"/>
+        <source>Please refer to &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
+        <translation>Se &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Opal.About.Common</name>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/DonationsGroup.qml" line="7"/>
+        <source>If you want to support my work, you can buy me a cup of coffee.</source>
+        <translation>Om du vill stödja mitt arbete får du gärna bjuda mig på en kopp kaffe.</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/DonationsGroup.qml" line="8"/>
+        <source>You can support this project by contributing, or by donating using any of these services.</source>
+        <translation>Du kan stödja detta projekt genom att bidra med kod eller genom att donera via någon av dessa tjänster.</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/DonationsGroup.qml" line="9"/>
+        <source>Your contributions to translations or code would be most welcome.</source>
+        <translation>Dina bidrag till översättningar eller kod skulle vara mycket välkomna.</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-laundry</name>
+    <message>
+        <location filename="../qml/harbour-laundry.qml" line="16"/>
+        <source>Laundry</source>
+        <translation>Tvätt</translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-laundry.qml" line="18"/>
+        <source>d M yyyy</source>
+        <translation>d M yyyy</translation>
+    </message>
+</context>
+</TS>
